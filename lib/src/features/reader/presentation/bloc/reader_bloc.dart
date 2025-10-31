dd532c116f21efea039c0435d51bdac92e875954
DrkXo <chayantaposh@gmail.com>
2025-10-31 22:51:38 +0530

    Import Fixes

--- lib/src/features/reader/presentation/bloc/reader_bloc.dart
+++ lib/src/features/reader/presentation/bloc/reader_bloc.dart
@@ -1,15 +1,12 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  import 'package:flutter_bloc/flutter_bloc.dart';
-  import 'package:freezed_annotation/freezed_annotation.dart';
-  
-  part 'reader_event.dart';
-  part 'reader_state.dart';
-  part 'reader_bloc.freezed.dart';
-  
-  class ReaderBloc extends Bloc<ReaderEvent, ReaderState> {
-    ReaderBloc() : super(_Initial()) {
-      on<ReaderEvent>((event, emit) {
-        // TODO: implement event handler
-      });
-    }
+part 'reader_bloc.freezed.dart';
+part 'reader_event.dart';
+part 'reader_state.dart';
+
+class ReaderBloc extends Bloc<ReaderEvent, ReaderState> {
+  ReaderBloc() : super(_Initial()) {
+    on<ReaderEvent>((event, emit) {});
   }
+}
--- lib/src/features/reader/presentation/pages/pages.dart
+++ lib/src/features/reader/presentation/pages/pages.dart
@@ -1,15 +1,14 @@
+import 'package:flutter/material.dart';
 
-    import 'package:flutter/material.dart';
-    class ReaderPage extends StatelessWidget {
-        const ReaderPage({Key? key}) : super(key: key);
-      
-        @override
-        Widget build(BuildContext context) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Reader Page'),
-            ),
-            );
-            }
-        }
-    
+class ReaderPage extends StatelessWidget {
+  const ReaderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reader Page'),
+      ),
+    );
+  }
+}
