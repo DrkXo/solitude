539b4b6568e6a325e9b50733135394efd4663f00
DrkXo <chayantaposh@gmail.com>
2025-11-08 10:50:20 +0530

    Work In Progress

--- lib/src/features/reader/presentation/widgets/reader_bottom_appbar.dart
+++ lib/src/features/reader/presentation/widgets/reader_bottom_appbar.dart
@@ -34,16 +34,8 @@ class ReaderBottomAppBar extends StatelessWidget {
                       Theme.of(context).primaryColor,
                   child: Row(
                     children: [
-                      TextButton(
-                        onPressed: () => _showChapterDialog(
-                          context,
-                          controller.totalChapters,
-                          currentChapterIndex,
-                          pageController!,
-                        ),
-                        child: Text(
-                          'Chapter ${currentChapterIndex + 1} of ${controller.totalChapters}',
-                        ),
+                      Text(
+                        'Chapter ${currentChapterIndex + 1} of ${controller.totalChapters}',
                       ),
                       const Spacer(),
                       IconButton(
@@ -77,74 +69,4 @@ class ReaderBottomAppBar extends StatelessWidget {
     );
   }
 
-  void _showChapterDialog(
-    BuildContext context,
-    int totalChapters,
-    int currentChapterIndex,
-    PageController pageController,
-  ) {
-    showModalBottomSheet(
-      context: context,
-      showDragHandle: true,
-      builder: (BuildContext context) {
-        return StatefulBuilder(
-          builder: (context, setState) {
-            ScrollController scrollController = ScrollController();
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              double height = MediaQuery.of(context).size.height * 0.8;
-              double itemHeight = 56.0;
-              double offset =
-                  currentChapterIndex * itemHeight -
-                  (height / 2) +
-                  (itemHeight / 2);
-              if (offset < 0) offset = 0;
-              scrollController.animateTo(
-                offset,
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeInOut,
-              );
-            });
-            return SizedBox(
-              height: MediaQuery.of(context).size.height * 0.8,
-              child: Column(
-                children: [
-                  Expanded(
-                    child: ListView.builder(
-                      controller: scrollController,
-                      itemCount: totalChapters,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          key: Key('chapter_$index'),
-                          leading: index == currentChapterIndex
-                              ? Padding(
-                                  padding: const EdgeInsets.only(right: 8.0),
-                                  child: Icon(
-                                    LucideIcons.bookOpen,
-                                    color: Theme.of(
-                                      context,
-                                    ).colorScheme.primary,
-                                  ),
-                                )
-                              : null,
-                          title: Text('Chapter ${index + 1}'),
-                          selected: index == currentChapterIndex,
-                          onTap: () {
-                            context.read<ReaderBloc>().add(
-                              ReaderEvent.updateReadingProgress(index),
-                            );
-                            pageController.jumpToPage(index);
-                            Navigator.of(context).pop();
-                          },
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-        );
-      },
-    );
-  }
 }
